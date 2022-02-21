3d3ccff15b62db051b52492c119f46f821792e12
László Kulhanek <klaszlo568@gmail.com>
2022-02-21 21:32:16 +0100

    profil böngésző nagyon alap
    minden szükséges adat meg van sql parancsok nélkül (de legalább lebírtam ellenőrizni)

--- controller/browser.php
+++ controller/browser.php
@@ -1,3 +1,48 @@
 <?php
+    if(empty($_SESSION["id"])){
+        header('Location: index.php?page=404');
+        exit();
+    }
+    $driver = new Driver();
+    $company = new Company();
+    $country = new Country();
+    $town = new Town();
+    $nearList = array();
+    $otherList = array();
+    if($_SESSION["type"]=="driver"){
+        $driver->set_user($_SESSION["id"], $conn);
+        $town->set_town($driver->get_townID(),$conn);
+        $country->set_country($town->get_country(),$conn);
+        $sqlGetNear = 'SELECT compID FROM companies INNER JOIN towns ON towns.townID=companies.townID WHERE countryID LIKE "'.$country->get_id().'"';
+        if($result = $conn->query($sqlGetNear)) {
+            if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+                    $nearList[] = $row['compID'];
+                }
+            }
+        }
+        foreach ($company->companiesList($conn) as $item){
+            if(!in_array($item,$nearList)){
+                $otherList[] = $item;
+            }
+        }
+    }else if($_SESSION["type"]=="company"){
+        $company->set_user($_SESSION["id"], $conn);
+        $town->set_town($company->get_townID(),$conn);
+        $country->set_country($town->get_country(),$conn);
+        $sqlGetNear = 'SELECT driverID FROM drivers INNER JOIN towns ON towns.townID=companies.townID WHERE countryID LIKE "'.$country->get_id().'"';
+        if($result = $conn->query($sqlGetNear)) {
+            if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+                    $nearList[] = $row['driverID'];
+                }
+            }
+        }
+        foreach ($driver->driversList($conn) as $item){
+            if(!in_array($item,$nearList)){
+                $otherList[] = $item;
+            }
+        }
+    }
     include 'view/browser.php';
 ?>
--- view/browser.php
+++ view/browser.php
@@ -2,8 +2,14 @@
 <p>1,Először az egy országban lévőket mutatja meg</p>
 <p>2,Utána a többit</p>
 <p>Megadja hogy az adott személy milyen országban van.</p>
-<code>SELECT countryID FROM `drivers` INNER JOIN towns ON towns.townID=drivers.townID WHERE driverID=2;</code>
+<code>echo $country->get_id();</code>
 <p>Azok az ID-k ahol megegyezik az ország</p>
 <code>SELECT compID FROM `companies` INNER JOIN towns ON towns.townID=companies.townID WHERE countryID LIKE "DEU"; </code>
 <p>Utána jöhet a többi ami esetleg nem abban az országban van mint a soför</p>
 <code>SELECT compID FROM `companies` INNER JOIN towns ON towns.townID=companies.townID WHERE countryID NOT LIKE "DEU";</code>
+<?php
+echo "Near List";
+print_r($nearList);
+echo "Other List";
+print_r($otherList);
+?>
