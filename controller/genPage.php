6e57565b85eb3458da1e0e62e7ddae39a36cc734
László Kulhanek <klaszlo568@gmail.com>
2022-03-10 10:47:04 +0100

    QR kód sofőröknek

--- controller/genPage.php
+++ controller/genPage.php
@@ -4,5 +4,67 @@
         header('Location: index.php?page=404');
         exit();
     }
+    $htmlFile = "<!DOCTYPE html>\n<html lang='hu'>\n<head>\n
+        <meta charset='utf-8'>\n
+        <link rel='stylesheet' type='text/css' href='../css/style.css'>\n
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>\n
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>\n
+        <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'></script>\n
+        <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js'></script>\n
+    ";
+    $driver = new Driver();
+    $company = new Company();
+    $country = new Country();
+    $town = new Town();
+    $fname = "";
+    if($_SESSION["type"]=="driver"){
+        $driver->set_user($_SESSION["id"], $conn);
+        $town->set_town($driver->get_townID(),$conn);
+        $country->set_country($town->get_country(),$conn);
+        $fname = md5($driver->get_username())."D";
+        if(isset($_POST["nameQ"])){
+            if($_POST["nameQ"]=="full"){
+                $htmlFile .= "<title>".$driver->get_lastname()." ".$driver->get_firstname()."</title>\n</head>\n<body class='bbody'>\n";
+                $htmlFile .= "<h1>".$driver->get_lastname()." ".$driver->get_firstname()."</h1>\n";
+            }else if($_POST["nameQ"]=="short"){
+                $htmlFile .= "<title>".substr($driver->get_lastname(), 0, 1).". ".$driver->get_firstname()."</title>\n</head>\n<body class='bbody'>\n<div class='container'>\n";
+                $htmlFile .= "<h1>".substr($driver->get_lastname(), 0, 1).". ".$driver->get_firstname()."</h1>\n";
+            }
+            if(isset($_POST["country"])){
+                $htmlFile .= "<p>Ország: ".$country->get_name()."</p>\n";
+            }
+            if(isset($_POST["town"])){
+                $htmlFile .= "<p>Város: ".$town->get_name()."</p>\n";
+            }
+            if(isset($_POST["street"])){
+                $htmlFile .= "<p>Utca: ".$driver->get_street()."</p>\n";
+            }
+            if(isset($_POST["houseNum"])){
+                $htmlFile .= "<p>Házszám: ".$driver->get_houseNumber()."</p>\n";
+            }
+            if(isset($_POST["email"])){
+                $htmlFile .= "<p>E-mail: ".$driver->get_email()."</p>\n";
+            }
+            if(isset($_POST["phone"])){
+                $htmlFile .= "<p>Telefonszám: ".$driver->get_phone()."</p>\n";
+            }
+            $htmlFile .= "</div>\n</body>\n</html>";
+            $myfile = @fopen("./generatedPages/".$fname.".html", "w") or die("Critical ERROR!");
+            fwrite($myfile, $htmlFile);
+            fclose($myfile);
+            header('Location: index.php?page=myProfile');
+            exit();
+        }else{
+            echo "<div class='alert alert-danger alert-dismissible'>
+            <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+            <p>A névnél kérem válassza ki a formátumot!</p>
+            </div>";
+        }
+    }else{
+        $company->set_user($_SESSION["id"], $conn);
+        $town->set_town($company->get_townID(),$conn);
+        $country->set_country($town->get_country(),$conn);
+        $fname = md5($company->get_username())."C";
+    }
     include 'view/genPage.php';
 ?>
